2a88d93002437c550dbb74240d5e24eb39255263
kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj <66171346+kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj@users.noreply.github.com>
2022-05-03 16:34:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function timed() {
+	window.setTimeout(function() {
+		
+	}, 2000);	
+}
+
+
+ function getE(ee) { document.querySelector(ee); }
+ function getV(ee) { document.querySelector(ee).value; }
+
+
+function sys(ee) {
+	
+}
+
+
+window.addEventListener("load", function() { 
+window.setTimeout(function() {
+		//document.querySelector('#loader').style.top="-100%";
+		document.querySelector('#loader').style.opacity="0";
+		}, 2000);
+ });
