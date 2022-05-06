eec3a479392a935e896f37eead624182f6ba8dd6
kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj <66171346+kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj@users.noreply.github.com>
2022-05-06 02:19:12 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function timed() {
 }
 
 
- function getE(ee) { document.querySelector(ee); }
- function getV(ee) { document.querySelector(ee).value; }
+ function getE(ee) { return document.querySelector(ee); }
+ function getV(ee) { return document.querySelector(ee).value; }
 
 
 function sys(ee) {
@@ -14,7 +14,25 @@ function sys(ee) {
 }
 
 
-window.addEventListener("load", function() { 
+window.addEventListener("load", function() {
+//all codes
+
+ getE('#mcart').addEventListener("focus",function() { getE('#cart').setAttribute("style","width:100%;height:100%;"); });
+ document.querySelector('#mmcart').addEventListener("focus",function() { getE('#cart').setAttribute("style","width:100%;height:100%;"); });
+ document.querySelector('#tmcart').addEventListener("focus",function() { getE('#cart').setAttribute("style","width:100%;height:100%;"); });
+ getE('#closecartB').addEventListener("focus",function() { getE('#cart').setAttribute("style","width:0px;height:0px;");  });
+ window.addEventListener("resize",function() { getE('#mom').setAttribute("style","height:"+ screen.height + "px;");  });
+ window.addEventListener("orientationchange",function() { getE('#mom').setAttribute("style","height:"+ screen.height + "px;");  });
+ window.addEventListener("resize",function() { getE('#tom').setAttribute("style","height:"+ screen.height + "px;");  });
+ window.addEventListener("orientationchange",function() { getE('#tom').setAttribute("style","height:"+ screen.height + "px;");  });
+ window.addEventListener("pageshow",function() { getE('#tom').setAttribute("style","height:"+ screen.height + "px;");  });
+ window.addEventListener("pageshow",function() { getE('#mom').setAttribute("style","height:"+ screen.height + "px;");  });
+
+
+
+
+
+	
 window.setTimeout(function() {
 		document.querySelector('#loader').style.opacity="0";
 		window.setTimeout(function() {
@@ -22,3 +40,5 @@ window.setTimeout(function() {
 		}, 2000);
 		}, 2000);
  });
+ 
+ 
