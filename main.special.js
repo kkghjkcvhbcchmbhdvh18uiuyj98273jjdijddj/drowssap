c023c29861fb78bcdf435fd427aa05d656746cee
kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj <66171346+kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj@users.noreply.github.com>
2022-05-10 21:40:20 +0200

    Add files via upload

--- main.special.js
+++ main.special.js
@@ -1,8 +1,20 @@
-function items(ee,bb)
+function items(ee,bb,tt)
  {
 	 if(bb=="add") {
 	 ee.previousSibling.innerHTML = (ee.previousSibling.innerHTML*1)+1;
 	 } else if(bb=="subtract" && ee.nextSibling.innerHTML>1){
 		ee.nextSibling.innerHTML = (ee.nextSibling.innerHTML*1)-1; 
 	 }
+	 getE('#total').innerHTML = tt*getE('.countz').innerHTML;
+ }
+ 
+ 
+ function preview(ee) {
+	 getE('.previews .selected').setAttribute("class",getE('.previews .selected').getAttribute("class").replace(" selected",""));
+	 ee.setAttribute("class", ee.getAttribute("class") + " selected");
+	 getE('.prod').style.opacity = "0.1";
+	 window.setTimeout(function() {
+	 getE('.prod').src = ee.getAttribute("href");
+	 getE('.prod').style.opacity = "1";  } , 1000);
+	 
  }
