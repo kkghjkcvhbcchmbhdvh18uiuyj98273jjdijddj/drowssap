f469dc05e4ef4e42554559d41c70c8377e23931d
kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj <66171346+kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj@users.noreply.github.com>
2022-05-11 10:28:43 +0200

    Add files via upload

--- main.special.js
+++ main.special.js
@@ -26,7 +26,7 @@ function items(ee,bb,tt)
 	 
 	 if(!localStorage.getItem('cartlist')) {
 		 i = 0;
-		  window.localStorage.setItem('cartlist','<div class="item">' + "<div class='ip' style='background-image:url("+ ip +");'>" + '</div><div class="itemd"><div class="ih">' + ih + '</div><div class="im">Quantity: ' + im + '</div><div class="it">Price: ' + it + '</div></div><div style="display:none" class="id">' + id + '</div></div>');
+		  window.localStorage.setItem('cartlist','<div class="item">' + "<div class='ip' style='background-image:url("+ ip +");'>" + '</div><div class="itemd"><div class="ih">' + ih + '</div><div class="im">Quantity: ' + im + '</div><div class="it">Price: R' + it + '</div></div><div style="display:none" class="id">' + id + '</div></div>');
 	 } else {
 		 i++;
 	 window.localStorage.setItem('cartlist',localStorage.getItem('cartlist') +  '<div class="item">' + "<div class='ip' style='background-image:url("+ ip +");'>" + '</div><div class="itemd"><div class="ih">' + ih + '</div><div class="im">Quantity: ' + im + '</div><div class="it">Price: ' + it + '</div></div><div style="display:none" class="id">' + id + '</div></div>');
@@ -35,7 +35,7 @@ function items(ee,bb,tt)
    ee.setAttribute("onclick",'');
    
    getE('#cartlist').innerHTML = localStorage.getItem('cartlist');
-   
+   getE('#closecartC').innerHTML = (getE('#closecartC').innerHTML*1)+(it*1);
    //localStorage.banpage
    //localStorage.cartcount
    
