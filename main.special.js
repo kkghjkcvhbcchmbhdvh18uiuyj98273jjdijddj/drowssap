ad9f6b84eed2bd5f60ef29aa903eac2de42d65b9
kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj <66171346+kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj@users.noreply.github.com>
2022-05-24 18:34:34 +0200

    Update main.special.js

--- main.special.js
+++ main.special.js
@@ -69,8 +69,8 @@ function checkForm(ee,ii) {
 	}
 	string = "";
 	ins = ee.getElementsByTagName('input');
-        if(ee.getElememtsByTagName('textarea')[0]) 
-       { ins[ins.length] = ee.getElememtsByTagName('textarea'); }
+        if(ee.querySelector('textarea')) 
+       { ins[ins.length] = ee.getElememtsByTagName('textarea')[0]; }
 	for(let i = 0; i<ins.length; i++) {
 		string += '&' + ins[i].getAttribute('name') + '=' + ins[i].value + '';
 		if(ii=="update") {
