35b1f58c3434a12584c849601d035b08ed6d6c8b
kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj <66171346+kkghjkcvhbcchmbhdvh18uiuyj98273jjdijddj@users.noreply.github.com>
2022-06-14 17:03:59 +0200

    Add files via upload

--- main.cart.js
+++ main.cart.js
@@ -9,6 +9,37 @@ if(!localStorage.getItem('cartlist')) {
 }
 document.body.innerHTML += '<div class="center" id="cart"><div class="center box"><div class="" id="cartlist">' + inic + '</div><button class="nbg brn" id="closecartB">Hide</button><button class="nbg brn" id="closecartC">' + init + '</button><button class="mbg brm" id="checkoutB">Checkout</button></div></div>';
 
+ function buyNow(ee,ih,id,ip,im,it) {
+	  
+	 details = id;
+   if(!localStorage.getItem('reg')) {
+	   localStorage.setItem('reg',0)
+   } else {
+	   localStorage.setItem('reg',(localStorage.getItem('reg')*1)+1);
+   }
+   id = localStorage.getItem('reg');
+   
+		 getE('#cartlist').innerHTML = '<div id="emptycart">Cart is empty.</div>';
+		 getE('#closecartC').innerHTML ="0.00";
+		 localStorage.setItem('reg',0);
+		 localStorage.removeItem('cartlist');
+	 
+	 
+	 if(!localStorage.getItem('cartlist')) {
+		 
+		  window.localStorage.setItem('cartlist','<div class="item" tabindex="2" id="reg' + id + '" tabindex="2"  style="overflow-y:auto;">' + "<div class='ip' style='background-image:url("+ ip +");'>" + '</div><div class="itemd"><div class="ih">' + ih + '</div><div class="im">Quantity: ' + im + '</div><div class="it">Price: R' + it + '</div><div class="ir" tabindex="2" onfocus="removeCart(this.parentNode.parentNode)">Remove</div></div><div style="display:none" class="id" reg="' + id + '">' + id + '</div></div>');
+    cartX = `@(@` + ih + `@,@` + details + `@,@` + ip + `@,@` + im + `@,@` + it + `@)@`;
+   localStorage.setItem('cartX',cartX);
+		  }
+		  
+   getE('#cartlist').innerHTML = localStorage.getItem('cartlist');
+   
+   getE('#closecartC').innerHTML = (getE('#closecartC').innerHTML*1)+(it*1);
+   
+   localStorage.setItem('total',getE('#closecartC').innerHTML);
+cartnot();
+ }
+
  function addToCart(ee,ih,id,ip,im,it) {
 	 
 	 details = id;
@@ -70,23 +101,39 @@ cartnot();
  
  
  
- getE('#checkoutB').addEventListener('click',function() {
+ getE('#checkoutB').addEventListener('click',function(){ checkOutNow();} );
+ 
+			function checkOutNow() {
 	 if(localStorage.getItem('total')=="0.00" || !localStorage.getItem('total')) {  /**do nothing**/  }
 	else {
 		fixList();
 		if(!localStorage.getItem('user')) {
 		window.location = 'Login?do=buying';  }
 		else {
-payfast = `<div id="payfast" style="display:none;"><form name="PayFastPayNowForm" action="https://www.payfast.co.za/eng/process" method="post">  <input required type="hidden" name="cmd" value="_paynow">  <input required type="hidden" name="receiver" pattern="[0-9]" value="19754082">  <input type="hidden" name="return_url" value="http://request.bleedingedgelab.co.za/man.php?ref=buy&status=success">  <input type="hidden" name="cancel_url" value="http://request.bleedingedgelab.co.za/man.php?ref=buy&status=cancel">  <input required type="hidden" name="amount" value="` + localStorage.getItem('total') + `" id="injtotal">  <input required type="hidden" name="item_name" maxlength="255" value="Bleeding Edge Lab (PTY) Ltd - Checkout">  <input type="hidden" name="item_description" maxlength="255" value="Checkout item(s): ` + localStorage.getItem('itemlist') + `">  <table>  <tr>  <td colspan=2 align=center>  <input type="image" src="https://www.payfast.co.za/images/buttons/PayNow/Dark-Large-Square-PayNow.png" alt="Pay Now" title="Pay Now with PayFast" id="payfastgo">  </td>  </tr>  </table>  </form></div>`;
-		document.body.innerHTML = payfast;
-		
-		getE('#payfastgo').click();
+		location = "Payment";
 		}
 		//address
 		//email
 		//redirect to pay
 	}
- });
+
+			}
+ 
+function yap() {
+	 if(localStorage.getItem('total')=="0.00" || !localStorage.getItem('total')) {  /**do nothing**/  }
+	else {
+		
+payfast = `<div id="payfast" style="display:none;"><form name="PayFastPayNowForm" action="https://www.payfast.co.za/eng/process" method="post">  <input required type="hidden" name="cmd" value="_paynow">  <input required type="hidden" name="receiver" pattern="[0-9]" value="19754082">  <input type="hidden" name="return_url" value="http://request.bleedingedgelab.co.za/man.php?ref=buy&status=success">  <input type="hidden" name="cancel_url" value="http://request.bleedingedgelab.co.za/man.php?ref=buy&status=cancel">  <input required type="hidden" name="amount" value="` + localStorage.getItem('total') + `" id="injtotal">  <input required type="hidden" name="item_name" maxlength="255" value="Bleeding Edge Lab (PTY) Ltd - Checkout">  <input type="hidden" name="item_description" maxlength="255" value="Checkout item(s): ` + localStorage.getItem('itemlist') + `">  <table>  <tr>  <td colspan=2 align=center>  <input type="image" src="https://www.payfast.co.za/images/buttons/PayNow/Dark-Large-Square-PayNow.png" alt="Pay Now" title="Pay Now with PayFast" id="payfastgo">  </td>  </tr>  </table>  </form></div>`;
+		document.body.innerHTML = payfast;
+		
+		getE('#payfastgo').click();
+		
+		//address
+		//email
+		//redirect to pay
+	}
+ }
+
 
 function cartnot() {
  if(localStorage.getItem('total')=="0.00" || !localStorage.getItem('total')) {
@@ -146,6 +193,27 @@ function fixList() {
 }
 
 
-
+function fixSum() {
+	ult = "";
+	se = localStorage.getItem('cartX').split('(@');
+	for(let y = 1; se.length>y; y++) {
+		pr = se[y].split('@)');
+		par = pr[0];
+		pars = par.split('@,@');
+		ih = pars[0];
+		details = pars[1];
+		ip = pars[2];
+		im = pars[3];
+		it = pars[4];
+		id = "";
+		ult += '<div class="item" tabindex="2" id="reg' + id + '" tabindex="2"  style="overflow-y:auto;">' + "<div class='ip' style='background-image:url("+ ip +");'>" + '</div><div class="itemd"><div class="ih">' + ih + '</div><div class="im">Quantity: ' + im + '</div><div class="it">Price: R' + it + '</div><div class="ir" tabindex="2" onfocus=""> &#10003; Pending </div></div><div style="display:none" class="id" reg="' + id + '">' + id + '</div></div>';
+	}
+	
+	getE('#summary').innerHTML = ult + getE('#summary').innerHTML;
+	getE('.summarytotal').innerHTML = "Total: R" + localStorage.getItem('total');
+	getE('#idfer').innerHTML = "<b>Reference:</b> " + localStorage.getItem('user_idnumber');
+	getE('#adue').innerHTML = "<b>Amount Due:</b> R" + localStorage.getItem('total');
+	
+}
 
 	 
